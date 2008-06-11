637eb71b064516bd59b9e500a35cd716fcc2026d
rickg22 <rickg22@88dc3e08-d44c-0410-8c4c-a6b43b8eb93d>
2008-06-11 06:01:18 +0000

    * Added stub for VideoOutputDevice class.

---
+++ trunk/src/iomgr/videooutputdevice.cpp
@@ -0,0 +1,16 @@
+/***************************************************************
+ * Name:      videooutputdevice.h
+ * Purpose:   Implementation of the VideoOutputDevice class
+ * Author:    Ricardo Garcia (rick.g777 {at} gmail {dot} com)
+ * Created:   2008-05-09
+ * Copyright: Ricardo Garcia (rick.g777 {at} gmail {dot} com)
+ * License:   GPL version 3 or later
+ **************************************************************/
+
+#include "videooutputdevice.h"
+
+VideoOutputDevice::VideoOutputDevice() {
+}
+
+VideoOutputDevice::~VideoOutputDevice() {
+}
---
+++ trunk/src/iomgr/videooutputdevice.h
@@ -0,0 +1,19 @@
+/***************************************************************
+ * Name:      videooutputdevice.h
+ * Purpose:   Declaration    of the VideoOutputDevice class
+ * Author:    Ricardo Garcia (rick.g777 {at} gmail {dot} com)
+ * Created:   2008-05-09
+ * Copyright: Ricardo Garcia (rick.g777 {at} gmail {dot} com)
+ * License:   GPL version 3 or later
+ **************************************************************/
+
+#ifndef videooutputdevice_h
+#define videooutputdevice_h
+
+class VideoOutputDevice {
+    public:
+        VideoOutputDevice();
+        virtual ~VideoOutputDevice();
+};
+
+#endif
