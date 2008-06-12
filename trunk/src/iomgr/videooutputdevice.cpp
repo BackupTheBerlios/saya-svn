8e27913de544e8f603a829268638db2cbc6cb56e
rickg22 <rickg22@88dc3e08-d44c-0410-8c4c-a6b43b8eb93d>
2008-06-12 03:22:17 +0000

    * Created VideoOutputDevice class. Mutex and sleep areas are marked, but they must be implemented. TODO: Make AudioOutputDevice class thread-safe.

--- trunk/src/iomgr/videooutputdevice.cpp
+++ trunk/src/iomgr/videooutputdevice.cpp
@@ -9,8 +9,122 @@
 
 #include "videooutputdevice.h"
 
-VideoOutputDevice::VideoOutputDevice() {
+/** Maximum size for the rendered window. 100 megapixels ought to be enough for anyone. */
+const unsigned int MaxVideoOutputDeviceWidth = 10240;
+const unsigned int MaxVideoOutputDeviceHeight = 10240;
+
+VideoOutputDevice::VideoOutputDevice() :
+m_colorformat(vcfRGB32),
+m_width(0),
+m_height(0),
+m_playing(false),
+m_changingsize(false),
+m_shuttingdown(false),
+m_ok(false)
+{
 }
 
 VideoOutputDevice::~VideoOutputDevice() {
+    ShutDown();
+}
+
+bool VideoOutputDevice::Init() {
+    if(!m_ok) {
+        m_ok = InitializeOutput();
+    }
+    return m_ok;
+}
+
+bool VideoOutputDevice::IsOk() {
+    return m_ok;
+}
+
+bool VideoOutputDevice::MustAbortPlayback() {
+    return (!m_ok || m_shuttingdown || m_changingsize);
+}
+
+void VideoOutputDevice::ShutDown() {
+    m_shuttingdown = true;
+    while(m_playing) {
+        // TODO: Sleep for 10 milliseconds
+    }
+    Clear();
+    DisconnectOutput();
+    m_ok = false;
+    m_shuttingdown = false;
+}
+
+unsigned int VideoOutputDevice::GetWidth() {
+    return m_width;
+}
+
+unsigned int VideoOutputDevice::GetHeight() {
+    return m_height;
+}
+
+bool VideoOutputDevice::ChangeSize(unsigned int newwidth,unsigned int newheight) {
+    bool result = true;
+
+    // TODO: Begin mutex here
+    if(m_shuttingdown || m_changingsize || m_playing || newwidth > MaxVideoOutputDeviceWidth || newheight > MaxVideoOutputDeviceHeight) {
+        result = false;
+    } else {
+        m_changingsize = true;
+    }
+    // TODO: End mutex here
+
+    if(result) {
+        result = ChangeDeviceSize(newwidth, newheight);
+        if(result) {
+            m_width = newwidth;
+            m_height = newheight;
+        }
+        m_changingsize = false;
+    }
+    return result;
+}
+
+bool VideoOutputDevice::ChangeDeviceSize(unsigned int newwidth,unsigned int newheight) {
+    // This is a stub
+    return false;
+}
+
+void VideoOutputDevice::LoadVideoData(VideoColorFormat colorformat, const char *buf,unsigned int buflen) {
+    bool result = true;
+    // TODO: Begin mutex here
+    if(m_playing || MustAbortPlayback()) {
+        result = false;
+    } else {
+        m_playing = true;
+    }
+    // TODO: End mutex here
+
+    if(result) {
+        LoadDeviceVideoData(colorformat,buf,buflen);
+        m_playing = false;
+    }
+}
+
+void VideoOutputDevice::LoadDeviceVideoData(VideoColorFormat colorformat, const char *buf,unsigned int buflen) {
+    // This is a stub
+}
+
+bool VideoOutputDevice::InitializeOutput() {
+    // This is a stub
+    m_width = 640;
+    m_height = 480;
+    m_colorformat = vcfRGB32;
+    return false;
+}
+
+void VideoOutputDevice::Clear() {
+    // This is a stub
+}
+
+void VideoOutputDevice::DisconnectOutput() {
+    // This is a stub
+}
+
+void VideoOutputDevice::RenderData() {
+    // This is a stub
 }
--- trunk/src/iomgr/videooutputdevice.h
+++ trunk/src/iomgr/videooutputdevice.h
@@ -10,10 +10,154 @@
 #ifndef videooutputdevice_h
 #define videooutputdevice_h
 
+/** enumerates the Video Color formats that our device can render / encode */
+enum VideoColorFormat {
+    vcfRGB8,
+    vcfRGB32,
+    vcfRGB24,
+    vcfRGB16,
+    vcfRGB15,
+    vcfBGR15,
+    vcfBGR16,
+    vcfBGR24,
+    vcfBGR32,
+    vcfYUY2,
+    vcfYUV,
+    vcfYV12,
+    vcfYUV12,
+    vcfYVYU,
+    vcfUYVY,
+    vcfYUY9,
+    vcfY800
+};
+
+/**
+ * @brief Generic wrapper for a Video Output device.
+ */
 class VideoOutputDevice {
     public:
+        /** Standard Constructor */
         VideoOutputDevice();
+
+        /** @brief Initializes the output device and sets the m_ok flag.
+         *
+         *  @note This is a wrapper for the InitializeOutput() virtual method.
+         *  @return True on success; false otherwise.
+         */
+        bool Init();
+
+         /** Returns the OK status for the output device. */
+        bool IsOk();
+
+        /** @brief Clears and disconnects the output device. Sets m_ok to false.
+          *
+          * @note This method can be called ONLY by the main thread!
+          */
+        void ShutDown();
+
+        /** Returns the output color format. */
+        VideoColorFormat GetColorFormat();
+
+        /** Returns the output width, in pixels. */
+        unsigned int GetWidth();
+
+        /** Returns the output height, in pixels. */
+        unsigned int GetHeight();
+
+        /** @brief Called whenever the output screen size is changed (i.e. by resizing the playback window)
+         *
+         *  @param newwidth the new width to be set for playback
+         *  @param newheight the new height to be set for playback
+         *  @return true if size was successfully changed, false otherwise.
+         *  @note This is a wrapper for the protected function ChangeDeviceSize(). After change takes place, variables
+         *  m_width and m_height are updated.
+         *  @note This method can be called ONLY by the main thread!
+         */
+        bool ChangeSize(unsigned int newwidth,unsigned int newheight);
+
+        /** @brief Loads video data from an external buffer.
+         *
+         *  @param colorformat The input color format being sent.
+         *  @param buf Buffer containing the data to be processed.
+         *  @param buflen The length of the buffer to be processed, in bytes.
+         *  @note  This method is a wrapper for LoadDeviceVideoData.
+         *  @note  This method should be called by the worker thread
+         */
+        void LoadVideoData(VideoColorFormat colorformat, const char *buf,unsigned int buflen);
+
+        /** Standard destructor. */
         virtual ~VideoOutputDevice();
+
+
+    protected:
+
+        /** @brief Initializes the output device.
+          *
+          * @note The m_ok flag MUST be set by this method.
+          * @return True on success; false otherwise.
+          * @note This method MUST set the m_width, m_height and m_colorformat variables.
+          */
+        virtual bool InitializeOutput();
+
+        /** Clears the video output device (i.e. sets it all to black). Called by ShutDown(). */
+        virtual void Clear();
+
+        /** Disconnects the video output device or memory resource. Called by ShutDown(). */
+        virtual void DisconnectOutput();
+
+        /** @brief Flag indicating that playback must be aborted immediately.
+          * @return true if playback/encoding thread must be aborted; false otherwise.
+          * @note This function MUST be called by LoadDeviceVideoData.
+          */
+        bool MustAbortPlayback();
+
+        /** @brief Called by ChangeSize whenever the output screen size is changed.
+          *
+          * This method must deal with size changes so that it resizes the buffers used for playback.
+          * @param newwidth the new width to be set for playback
+          * @param newheight the new height to be set for playback
+          * @return true if size was changed, false otherwise.
+          */
+        virtual bool ChangeDeviceSize(unsigned int newwidth,unsigned int newheight);
+
+        /** @brief Loads video data from an external buffer.
+         *
+         *  @param colorformat The input color format being sent.
+         *  @param buf Buffer containing the data to be processed.
+         *  @param buflen The length of the buffer to be processed, in bytes.
+         *  @note  This method MUST do nothing if either m_width or m_height are set to 0.
+         *         When the data is finally converted, RenderData MUST be called.
+         *  @note  This function MUST check MustAbortPlayback() regularly and abort rendering when the result is true.
+         */
+        virtual void LoadDeviceVideoData(VideoColorFormat colorformat, const char *buf,unsigned int buflen);
+
+
+        /** Plays the received frames.
+          * @note This method MUST do nothing if either m_width or m_height are set to 0.
+          */
+        virtual void RenderData();
+
+        /** Output color format */
+        VideoColorFormat m_colorformat;
+
+        /** Output width */
+        unsigned int m_width;
+
+        /**  Output height */
+        unsigned int m_height;
+
+    private:
+
+        /** flag that indicates that playing is ACTUALLY taking place. */
+        bool m_playing;
+
+        /** flag that forbids playback when changing the device size */
+        bool m_changingsize;
+
+        /** flag that forbids playback when shutting down the device */
+        bool m_shuttingdown;
+
+        bool m_ok;                      /** Tells whether the output device was initialized correctly. */
 };
 
 #endif
