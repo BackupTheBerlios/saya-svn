c12bbb24c4f07a9a61d23fb7ff683bd044eafd5c
rigobertoc <rigobertoc@88dc3e08-d44c-0410-8c4c-a6b43b8eb93d>
2008-10-06 06:02:46 +0000

    Changes to thread tests project: 1.- Renamed SyThreadTest.cpp to Test1.cpp 2.- Added Test2.cpp same test as Test1 but asks the user to press enter before starting secondary thread. 3.- Added targets to test project test1.exe & test2.exe 4.- Renamed test project to ThreadTest.cbp

---
+++ trunk/src/threadtest/Test1.cpp
@@ -0,0 +1,80 @@
+/****************************************************************
+ * Name:      Test1.cpp
+ * Purpose:   Unit test for syThread class
+ * Author:    Rigoberto Calleja (rcallejac {at} gmail {dot} com)
+ * Date :     2008-09-24
+ * Copyright: (c) Rigoberto Calleja
+ * License:   WxWindows License
+ ***************************************************************/
+#include <iostream>
+#include "../iomgr/sythread.h"
+
+using namespace std;
+
+class Test1 : public syThread
+{
+public:
+    Test1();
+
+    // thread execution starts here
+    virtual int Entry();
+
+    virtual void OnExit();
+};
+
+Test1::Test1()
+             :syThread()
+{}
+
+int Test1::Entry()
+{
+    for ( ; ;  )
+    {
+        // check if we were asked to exit
+        if ( TestDestroy() )
+            break;
+        {
+            cout << "o" << flush;
+        }
+        syThread::Sleep(150);
+    }
+    return 0;
+}
+
+void Test1::OnExit() {
+    cout << "Secondary thread finished." << endl;
+}
+
+int main()
+{
+    Test1 *thread = new Test1();
+
+    if ( thread->Create() != syTHREAD_NO_ERROR )
+    {
+        cout << "Can't create secondary thread!\n";
+    }
+
+    if ( thread->Run() != syTHREAD_NO_ERROR )
+    {
+        cout << "Can't start secondary thread!\n";
+    }
+
+    cout << "Main thread running";
+    for(int i=0;i<100;i++){
+        syMilliSleep(100);
+        if(i == 20) {
+            thread->Pause();
+        }
+        if(i == 60) {
+            thread->Resume();
+        }
+        {
+            cout << "." << flush;
+        }
+    }
+    thread->Delete();
+    // What happens if we don't wait for the thread to finish?
+    // syMilliSleep(1000);
+
+    return 0;
+}
