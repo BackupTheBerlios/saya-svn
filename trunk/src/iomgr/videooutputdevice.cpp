e608f793013cbc96d58c850899660bd9d906cc43
rickg22 <rickg22@88dc3e08-d44c-0410-8c4c-a6b43b8eb93d>
2008-10-23 04:42:44 +0000

    * Made VideoOutputDevice::LoadVideoData and VideoOutputDevice::FlushVideoData lock-free with respect to each other.

--- trunk/src/iomgr/videooutputdevice.cpp
+++ trunk/src/iomgr/videooutputdevice.cpp
@@ -8,6 +8,7 @@
  **************************************************************/
 
 #include "sythread.h"
+#include "atomic.h"
 #include "videooutputdevice.h"
 #include "sybitmap.h"
 #include "sentryfuncs.h"
@@ -15,13 +16,27 @@
 const unsigned int VideoOutputDevice::MaxWidth = 10240;
 const unsigned int VideoOutputDevice::MaxHeight = 10240;
 
+// ---------------------------
+// Begin VideoOutputDeviceData
+// ---------------------------
+
 class VideoOutputDeviceData {
 
     public:
 
-        syBitmap* m_InputBitmap;
-        syBitmap* m_OutputBitmap;
-        syBitmap* m_ExtraBitmap;
+        syBitmap* volatile m_InputBitmap;
+        syBitmap* volatile m_OutputBitmap;
+        syBitmap* volatile m_ExtraBitmap;
+        // Using the form "volatile syBitmap* m_InputBitmap" would make the contents volatile.
+        // To make the pointer volatile we use the form  "syBitmap* volatile m_InputBitmap".
+
+        /** Flag indicating that there's new data present in m_ExtraBitmap */
+        volatile bool m_NewInput;
+
+        // So, we have m_InputBitmap, m_OutputBitmap, and m_ExtraBitmap.
+        // Our current model for VideoOutputDevice is that we have only one producer and only one consumer.
+        // This will simplify handling the buffer. Besides, we have m_InputMutex and m_OutputMutex inherited
+        // from AVDevice.
 
         VideoOutputDeviceData() {
             m_InputBitmap = new syBitmap;
@@ -34,7 +49,47 @@ class VideoOutputDeviceData {
             delete m_OutputBitmap;
             delete m_InputBitmap;
         }
+
+        void SwapInputAndExtraBitmaps();
+        void CheckForNewData();
+        void SwapOutputAndExtraBitmaps();
 };
+
+void VideoOutputDeviceData::SwapInputAndExtraBitmaps() {
+    syBitmap* tmpbmp;
+    // We need to spin for this operation since the output thread might swap the output and the extra
+    // bitmaps.
+    for(;;) {
+        tmpbmp =  m_ExtraBitmap;
+        if(syAtomic::bool_CAS((void**)(&m_ExtraBitmap),(void*)tmpbmp,(void*)(m_InputBitmap))) break;
+    }
+    m_InputBitmap = tmpbmp;
+    m_NewInput = true;
+    syAtomic::MemoryBarrier();
+}
+
+void VideoOutputDeviceData::SwapOutputAndExtraBitmaps() {
+    syBitmap* tmpbmp;
+    // We need to spin for this operation since the input thread might swap the input and extra
+    // bitmaps.
+    for(;;) {
+        tmpbmp =  m_ExtraBitmap;
+        if(syAtomic::bool_CAS((void**)(&m_ExtraBitmap),(void*)tmpbmp,(void*)(m_OutputBitmap))) break;
+    }
+    m_OutputBitmap = tmpbmp;
+    syAtomic::MemoryBarrier();
+}
+
+void VideoOutputDeviceData::CheckForNewData() {
+    if(syAtomic::bool_CAS((bool*)&m_NewInput,true,false)) {
+        // The input thread wrote into m_ExtraBitmap; we must swap and process the output.
+        SwapOutputAndExtraBitmaps();
+    }
+}
+
+// -------------------------
+// End VideoOutputDeviceData
+// -------------------------
 
 VideoOutputDevice::VideoOutputDevice() : AVDevice(),
 m_ColorFormat(vcfRGB32),
@@ -94,8 +149,11 @@ void VideoOutputDevice::LoadVideoData(const syBitmap* bitmap) {
 
     sySafeMutexLocker lock(*m_InputMutex, this);
     if(lock.IsLocked()) {
+        // Step one: Read data from bitmap into m_InputBitmap.
         m_Data->m_InputBitmap->PasteFrom(bitmap,sy_stkeepaspectratio);
-        // TODO: Read data from the source bitmap.
+
+        // Step two: Swap m_InputBitmap and m_ExtraBitmap.
+        m_Data->SwapInputAndExtraBitmaps();
     }
 }
 
@@ -104,6 +162,10 @@ void VideoOutputDevice::FlushVideoData() {
     if(MustAbort()) return;
     sySafeMutexLocker lock(*m_OutputMutex, this);
     if(lock.IsLocked()) {
+        // Step one: Swap the Extra bitmap and the output bitmap, if necessary
+        m_Data->CheckForNewData();
+
+        // Step two: Render the data from m_OutputBitmap into the actual device.
         RenderVideoData(m_Data->m_OutputBitmap);
     }
 }
