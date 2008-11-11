5819175d0d25146c234651b6b76694d57b439a50
rigobertoc <rigobertoc@88dc3e08-d44c-0410-8c4c-a6b43b8eb93d>
2008-11-11 01:29:02 +0000

    Second attempt to implement saya's playback control class

--- trunk/src/main/playcontrol.cpp
+++ trunk/src/main/playcontrol.cpp
@@ -1,17 +1,36 @@
 /****************************************************************************
  * Name:      playcontrol.h
- * Purpose:   Definition for Saya play control
+ * Purpose:   Definition for Saya playback control
  * Author:    Rigoberto Calleja (rcallejac {at} gmail {dot} com)
  * Created:   2008-11-10
  * Copyright: Rigoberto Calleja (rcallejac {at} gmail {dot} com)
  * License:   GPL version 3 or later
  * Comments:  Based on code found at
  *            http://zetcode.com/tutorials/wxwidgetstutorial/customwidgets/
+ *            http://zetcode.com/tutorials/wxwidgetstutorial/widgets/
  ***************************************************************************/
-#include "playcontrol.h"
 #include <wx/wx.h>
+#include "playcontrol.h"
+
+int ID_SLIDER = 1;
+
 PlayControl::PlayControl(wxPanel* parent, int id)
     : wxPanel(parent, id, wxDefaultPosition, wxSize(-1, -1), wxSUNKEN_BORDER)
 {
+    m_parent = parent;
 
-};
+    wxPanel *panel = new wxPanel(this, wxID_ANY);
+
+    m_slider = new wxSlider(panel, ID_SLIDER, 0, 0, 100, wxPoint(-1, -1),
+      wxSize(150, -1), wxSL_LABELS);
+
+    m_stop = new wxBitmapButton(panel, wxID_ANY, wxBitmap(wxT("../resources/img/new_video.png"),
+      wxBITMAP_TYPE_PNG), wxPoint(180, 20));
+
+    Connect(wxEVT_PAINT, wxPaintEventHandler(PlayControl::OnPaint));
+}
+
+void PlayControl::OnPaint(wxPaintEvent& event)
+{
+
+}
--- trunk/src/main/playcontrol.h
+++ trunk/src/main/playcontrol.h
@@ -1,12 +1,13 @@
 /****************************************************************************
  * Name:      playcontrol.h
- * Purpose:   Declaration for Saya play control
+ * Purpose:   Declaration for Saya playback control
  * Author:    Rigoberto Calleja (rcallejac {at} gmail {dot} com)
  * Created:   2008-11-10
  * Copyright: Rigoberto Calleja (rcallejac {at} gmail {dot} com)
  * License:   GPL version 3 or later
  * Comments:  Based on code found at
  *            http://zetcode.com/tutorials/wxwidgetstutorial/customwidgets/
+ *            http://zetcode.com/tutorials/wxwidgetstutorial/widgets/
  ***************************************************************************/
 
 #ifndef PLAYCONTROL_H_INCLUDED
@@ -15,7 +16,16 @@
 #include <wx/wx.h>
 class PlayControl : public wxPanel
 {
+private:
+    wxSlider *m_slider;
+    wxBitmapButton *m_stop;
+    wxBitmapButton *m_play;
+    wxBitmapButton *m_fastrewind;
+    wxBitmapButton *m_fastforward;
 public:
+    wxPanel *m_parent;
+
     PlayControl(wxPanel* parent, int id);
+    void OnPaint(wxPaintEvent& event);
 };
 #endif // PLAYCONTROL_H_INCLUDED
