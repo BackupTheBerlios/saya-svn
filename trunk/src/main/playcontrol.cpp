7b8ffa5ec74f407c2b21442a9a35567d4b84aa7d
rigobertoc <rigobertoc@88dc3e08-d44c-0410-8c4c-a6b43b8eb93d>
2008-11-11 00:50:14 +0000

    First shot at Saya's play control declaration and definition files

---
+++ trunk/src/main/playcontrol.cpp
@@ -0,0 +1,17 @@
+/****************************************************************************
+ * Name:      playcontrol.h
+ * Purpose:   Definition for Saya play control
+ * Author:    Rigoberto Calleja (rcallejac {at} gmail {dot} com)
+ * Created:   2008-11-10
+ * Copyright: Rigoberto Calleja (rcallejac {at} gmail {dot} com)
+ * License:   GPL version 3 or later
+ * Comments:  Based on code found at
+ *            http://zetcode.com/tutorials/wxwidgetstutorial/customwidgets/
+ ***************************************************************************/
+#include "playcontrol.h"
+#include <wx/wx.h>
+PlayControl::PlayControl(wxPanel* parent, int id)
+    : wxPanel(parent, id, wxDefaultPosition, wxSize(-1, -1), wxSUNKEN_BORDER)
+{
+
+};
---
+++ trunk/src/main/playcontrol.h
@@ -0,0 +1,21 @@
+/****************************************************************************
+ * Name:      playcontrol.h
+ * Purpose:   Declaration for Saya play control
+ * Author:    Rigoberto Calleja (rcallejac {at} gmail {dot} com)
+ * Created:   2008-11-10
+ * Copyright: Rigoberto Calleja (rcallejac {at} gmail {dot} com)
+ * License:   GPL version 3 or later
+ * Comments:  Based on code found at
+ *            http://zetcode.com/tutorials/wxwidgetstutorial/customwidgets/
+ ***************************************************************************/
+
+#ifndef PLAYCONTROL_H_INCLUDED
+#define PLAYCONTROL_H_INCLUDED
+
+#include <wx/wx.h>
+class PlayControl : public wxPanel
+{
+public:
+    PlayControl(wxPanel* parent, int id);
+};
+#endif // PLAYCONTROL_H_INCLUDED
