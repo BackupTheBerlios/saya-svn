baf4ac086d1fec58f7d633e1f7ed8a501147acc2
rigobertoc <rigobertoc@88dc3e08-d44c-0410-8c4c-a6b43b8eb93d>
2008-11-23 02:09:41 +0000

    Inside wxFormBuilder playback control project: a) Updated references to button images to reflect lowercase filenames. b) Updated wxFormBuilder project generated filenames and paths to: saya\src\main\wxplaybackcontrolpanel.cpp & .h

--- trunk/src/main/wxplaybackcontrolpanel.cpp
+++ trunk/src/main/wxplaybackcontrolpanel.cpp
@@ -1,36 +1,65 @@
-/****************************************************************************
- * Name:      playcontrol.h
- * Purpose:   Definition for Saya playback control
- * Author:    Rigoberto Calleja (rcallejac {at} gmail {dot} com)
- * Created:   2008-11-10
- * Copyright: Rigoberto Calleja (rcallejac {at} gmail {dot} com)
- * License:   GPL version 3 or later
- * Comments:  Based on code found at
- *            http://zetcode.com/tutorials/wxwidgetstutorial/customwidgets/
- *            http://zetcode.com/tutorials/wxwidgetstutorial/widgets/
- ***************************************************************************/
-#include <wx/wx.h>
-#include "playcontrol.h"
+///////////////////////////////////////////////////////////////////////////
+// C++ code generated with wxFormBuilder (version Apr 16 2008)
+// http://www.wxformbuilder.org/
+//
+// PLEASE DO "NOT" EDIT THIS FILE!
+///////////////////////////////////////////////////////////////////////////
 
-int ID_SLIDER = 1;
+#include "wxplaybackcontrolpanel.h"
 
-PlayControl::PlayControl(wxPanel* parent, int id)
-    : wxPanel(parent, id, wxDefaultPosition, wxSize(-1, -1), wxSUNKEN_BORDER)
+///////////////////////////////////////////////////////////////////////////
+
+AllInOnePlaybackControlGUI::AllInOnePlaybackControlGUI( wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style ) : wxPanel( parent, id, pos, size, style )
 {
-    m_parent = parent;
+	wxBoxSizer* vBox;
+	vBox = new wxBoxSizer( wxVERTICAL );
 
-    wxPanel *panel = new wxPanel(this, wxID_ANY);
+	wxBoxSizer* hbox1;
+	hbox1 = new wxBoxSizer( wxHORIZONTAL );
 
-    m_slider = new wxSlider(panel, ID_SLIDER, 0, 0, 100, wxPoint(-1, -1),
-      wxSize(150, -1), wxSL_LABELS);
+	m_playbackslider = new wxSlider( this, ID_PLAYSLIDER, 0, 0, 100, wxDefaultPosition, wxDefaultSize, wxSL_HORIZONTAL );
+	hbox1->Add( m_playbackslider, 1, wxEXPAND, 5 );
 
-    m_stop = new wxBitmapButton(panel, wxID_ANY, wxBitmap(wxT("../resources/img/new_video.png"),
-      wxBITMAP_TYPE_PNG), wxPoint(180, 20));
+	vBox->Add( hbox1, 1, wxEXPAND, 5 );
 
-    Connect(wxEVT_PAINT, wxPaintEventHandler(PlayControl::OnPaint));
+	wxBoxSizer* hbox2;
+	hbox2 = new wxBoxSizer( wxHORIZONTAL );
+
+	m_firstframe = new wxBitmapButton( this, ID_FIRSTFRAME, wxBitmap( wxT("../resources/img/btn_firstframe.png"), wxBITMAP_TYPE_ANY ), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW );
+	hbox2->Add( m_firstframe, 0, 0, 5 );
+
+	m_fastrewind = new wxBitmapButton( this, ID_FASTREWIND, wxBitmap( wxT("../resources/img/btn_fastrewind.png"), wxBITMAP_TYPE_ANY ), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW );
+	hbox2->Add( m_fastrewind, 0, 0, 5 );
+
+	m_previousframe = new wxBitmapButton( this, ID_PREVIOUSFRAME, wxBitmap( wxT("../resources/img/btn_prevframe.png"), wxBITMAP_TYPE_ANY ), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW );
+	hbox2->Add( m_previousframe, 0, 0, 5 );
+
+	m_play = new wxBitmapButton( this, ID_PLAY, wxBitmap( wxT("../resources/img/btn_play.png"), wxBITMAP_TYPE_ANY ), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW );
+	hbox2->Add( m_play, 0, 0, 5 );
+
+	m_nextframe = new wxBitmapButton( this, ID_NEXTFRAME, wxBitmap( wxT("../resources/img/btn_nextframe.png"), wxBITMAP_TYPE_ANY ), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW );
+	hbox2->Add( m_nextframe, 0, 0, 5 );
+
+	m_fastforward = new wxBitmapButton( this, ID_FASTFORWARD, wxBitmap( wxT("../resources/img/btn_fastforward.png"), wxBITMAP_TYPE_ANY ), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW );
+	hbox2->Add( m_fastforward, 0, 0, 5 );
+
+	m_lastframe = new wxBitmapButton( this, ID_LASTFRAME, wxBitmap( wxT("../resources/img/btn_lastframe.png"), wxBITMAP_TYPE_ANY ), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW );
+	hbox2->Add( m_lastframe, 0, 0, 5 );
+
+	m_shuttle = new wxSlider( this, ID_SHUTTLE, 50, 0, 100, wxDefaultPosition, wxDefaultSize, wxSL_HORIZONTAL );
+	hbox2->Add( m_shuttle, 0, 0, 5 );
+
+	m_display = new wxStaticText( this, wxID_ANY, wxT("[Display]"), wxDefaultPosition, wxDefaultSize, wxALIGN_RIGHT );
+	m_display->Wrap( -1 );
+	hbox2->Add( m_display, 1, 0, 5 );
+
+	vBox->Add( hbox2, 1, wxEXPAND, 5 );
+
+	this->SetSizer( vBox );
+	this->Layout();
+	vBox->Fit( this );
 }
 
-void PlayControl::OnPaint(wxPaintEvent& event)
+AllInOnePlaybackControlGUI::~AllInOnePlaybackControlGUI()
 {
-
 }
--- trunk/src/main/wxplaybackcontrolpanel.h
+++ trunk/src/main/wxplaybackcontrolpanel.h
@@ -1,31 +1,64 @@
-/****************************************************************************
- * Name:      playcontrol.h
- * Purpose:   Declaration for Saya playback control
- * Author:    Rigoberto Calleja (rcallejac {at} gmail {dot} com)
- * Created:   2008-11-10
- * Copyright: Rigoberto Calleja (rcallejac {at} gmail {dot} com)
- * License:   GPL version 3 or later
- * Comments:  Based on code found at
- *            http://zetcode.com/tutorials/wxwidgetstutorial/customwidgets/
- *            http://zetcode.com/tutorials/wxwidgetstutorial/widgets/
- ***************************************************************************/
+///////////////////////////////////////////////////////////////////////////
+// C++ code generated with wxFormBuilder (version Apr 16 2008)
+// http://www.wxformbuilder.org/
+//
+// PLEASE DO "NOT" EDIT THIS FILE!
+///////////////////////////////////////////////////////////////////////////
 
-#ifndef PLAYCONTROL_H_INCLUDED
-#define PLAYCONTROL_H_INCLUDED
+#ifndef __wxplaybackcontrolpanel__
+#define __wxplaybackcontrolpanel__
 
-#include <wx/wx.h>
-class PlayControl : public wxPanel
+#include <wx/slider.h>
+#include <wx/gdicmn.h>
+#include <wx/font.h>
+#include <wx/colour.h>
+#include <wx/settings.h>
+#include <wx/string.h>
+#include <wx/sizer.h>
+#include <wx/bitmap.h>
+#include <wx/image.h>
+#include <wx/icon.h>
+#include <wx/bmpbuttn.h>
+#include <wx/button.h>
+#include <wx/stattext.h>
+#include <wx/panel.h>
+
+///////////////////////////////////////////////////////////////////////////
+
+#define ID_DISPLAY 1000
+#define ID_PLAYSLIDER 1001
+#define ID_FIRSTFRAME 1002
+#define ID_FASTREWIND 1003
+#define ID_PREVIOUSFRAME 1004
+#define ID_PLAY 1005
+#define ID_NEXTFRAME 1006
+#define ID_FASTFORWARD 1007
+#define ID_LASTFRAME 1008
+#define ID_SHUTTLE 1009
+
+///////////////////////////////////////////////////////////////////////////////
+/// Class AllInOnePlaybackControlGUI
+///////////////////////////////////////////////////////////////////////////////
+class AllInOnePlaybackControlGUI : public wxPanel 
 {
-private:
-    wxSlider *m_slider;
-    wxBitmapButton *m_stop;
-    wxBitmapButton *m_play;
-    wxBitmapButton *m_fastrewind;
-    wxBitmapButton *m_fastforward;
-public:
-    wxPanel *m_parent;
-
-    PlayControl(wxPanel* parent, int id);
-    void OnPaint(wxPaintEvent& event);
+	private:
+	
+	protected:
+		wxSlider* m_playbackslider;
+		wxBitmapButton* m_firstframe;
+		wxBitmapButton* m_fastrewind;
+		wxBitmapButton* m_previousframe;
+		wxBitmapButton* m_play;
+		wxBitmapButton* m_nextframe;
+		wxBitmapButton* m_fastforward;
+		wxBitmapButton* m_lastframe;
+		wxSlider* m_shuttle;
+		wxStaticText* m_display;
+	
+	public:
+		AllInOnePlaybackControlGUI( wxWindow* parent, wxWindowID id = ID_DISPLAY, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( -1,-1 ), long style = wxTAB_TRAVERSAL );
+		~AllInOnePlaybackControlGUI();
+	
 };
-#endif // PLAYCONTROL_H_INCLUDED
+
+#endif //__wxplaybackcontrolpanel__
